7ac165aca16dc82c28bb8641677b5338dea19d3e
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-09-13 15:01:07 +0200

    Build on controller git commit -m "$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtehnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- demo/demo/src/main/java/Chilis/com/demo/Controller/Controller.java
+++ demo/demo/src/main/java/Chilis/com/demo/Controller/Controller.java
@@ -1,13 +1,26 @@
 package Chilis.com.demo.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin(origins = "*")
-@RequestMapping("/messenger")
+@RequestMapping("/fetch")
 @org.springframework.stereotype.Controller
 public class Controller {
     public Controller() {
 
+    }
+
+    @GetMapping
+    public List<String> findAll(){
+
+        List<String> tempList = new ArrayList<>();
+
+        tempList.add("This IS THE ENDPOINT");
+        return tempList;
     }
 }
