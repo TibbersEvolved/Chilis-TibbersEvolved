39af3692ba106c865178c5a643fa8abe8dad1a46
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-09-13 13:14:52 +0200

    dd submitlistener git commit -m "$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtehnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,12 @@
 const fetchButton = document.querySelector("#fetchButton");
+const fetchForm = document.querySelector("#fetchPart");
+
 
 fetchButton.onClick = () => {
   alert("hi");
 };
+
+fetchForm.addEventListener("click", function (e) {
+  alert("no problemo");
+})
+alert("this is amazing");
