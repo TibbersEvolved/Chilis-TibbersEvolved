510272eb6937763736dcf1391f6013303bb0eaa0
Sabine <sabinehernandes@gmail.com>
2024-09-13 13:25:08 +0200

    update eventlistener function git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtehnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,15 @@
 const fetchButton = document.querySelector("#fetchButton");
 const fetchForm = document.querySelector("#fetchPart");
-
+const path = "http://localhost:8080";
 
 fetchButton.onClick = () => {
   alert("hi");
 };
 
-fetchForm.addEventListener("click", function (e) {
+fetchButton.addEventListener("click", function (e) {
   alert("no problemo");
+  fetch(path + "/fetch")
+  .then((response) => response.json())
+  .then((data) => alert({data}));
 })
 alert("this is amazing");
