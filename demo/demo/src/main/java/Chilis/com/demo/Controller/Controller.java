2abaf7e6c1c4165a944ca7df850ec5f1a633658c
John Eliasson <john.eliasson@appliedtechnology.se>
2024-09-13 13:52:00 +0200

    Add controller class
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

---
+++ demo/demo/src/main/java/Chilis/com/demo/Controller/Controller.java
@@ -0,0 +1,13 @@
+package Chilis.com.demo.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/messenger")
+@org.springframework.stereotype.Controller
+public class Controller {
+    public Controller() {
+
+    }
+}
